4c902346ef872d4def1cb19059f0132fc257f7ec
joyy0103 <joyy0103@gmail.com>
2024-04-25 17:07:42 +0900

    20240425

--- apps/app.py
+++ apps/app.py
@@ -1,8 +1,31 @@
+from pathlib import Path
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+from apps.config import config
 
-def create_app():
+db = SQLAlchemy()
+csrf = CSRFProtect()
+
+def create_app(config_key):
 
     app = Flask(__name__)
+
+    app.config.from_object(config[config_key])
+
+    app.config.from_mapping(
+        SECRET_KEY="ajsdlfjaldsjakfj145jskaf",
+        SQLALCHEMY_DATABASE_URI=
+            f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        SQLALCHEMY_ECHO=True,
+        WTF_CSRF_SECRET_KEY="sakfjlasfj15364jaslk"
+    )
+
+    csrf.init_app(app)
+    db.init_app(app)
+    Migrate(app, db)
 
     from apps.crud import views as crud_views
 
--- apps/crud/views.py
+++ apps/crud/views.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+from apps.app import db
+from apps.crud.models import User
+from apps.crud.forms import UserForm
 
 crud = Blueprint(
     "crud",
@@ -10,3 +13,56 @@ crud = Blueprint(
 @crud.route("/")
 def index():
     return render_template("crud/index.html")
+
+@crud.route("/sql")
+def sql():
+    db.session.query(User).all()
+    return "콘솔 로그를 확인해 주세요"
+
+@crud.route("/users/new", methods=["GET", "POST"])
+def create_user():
+    form = UserForm()
+
+    if form.validate_on_submit():
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for("crud.users"))
+    return render_template("crud/create.html", form=form)
+
+@crud.route("/users")
+def users():
+    users = User.query.all()
+    return render_template("crud/index.html", users=users)
+
+@crud.route("/users/<user_id>", methods=["GET", "POST"])
+def edit_user(user_id):
+    form = UserForm()
+
+    # User 모델을 이용하여 사용자를 취득한다
+    user = User.query.filter_by(id=user_id).first()
+
+    # form으로부터 제출된 경우는 사용자를 갱신하여 사용자의 일람 화면으로 리다이렉트한다
+    if form.validate_on_submit():
+        user.username = form.username.data
+        user.email = form.email.data
+        user.password = form.password.data
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("crud.users"))
+
+    # GET의 경우는 HTML을 반환한다
+    return render_template("crud/edit.html", user=user, form=form)
+
+@crud.route("/users/<user_id>/delete", methods=["POST"])
+def delete_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for("crud.users"))
